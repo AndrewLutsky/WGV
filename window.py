7dc5ddc2b8a4177e99feaea1b0ed6f9e458405ba
Andrew Lutsky <andrew.lutsky19@gmail.com>
2022-10-26 09:21:07 -0500

    working on file explorer

--- window.py
+++ window.py
@@ -2,18 +2,34 @@ from PyQt5 import QtWidgets
 
 import sys
 
+def oFile():
+    print("What file would you like to open?")
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Whole Genome Viewer")
-        l = QtWidgets.QLabel("Visualizer")
-        l.setMargin(100)
-        self.setCentralWidget(l)
-        self.show()
+        self.setGeometry(0,0,1920,1080)
+        #Open File Button
+        ofile = QtWidgets.QPushButton(self)
+        ofile.setText("Open File")
+        ofile.clicked.connect(oFile)        
 
+        #shows main window
+        self.show()
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
+    
     app.exec()
+
+class oFileWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Open File")
+
+
+
+
